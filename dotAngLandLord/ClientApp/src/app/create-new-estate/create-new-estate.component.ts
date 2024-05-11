6f476308d110bd8b1719d0a460c7ece2c2f02bac
dawidsip <dawidsip@gmail.com>
2024-05-12 01:14:48 +0200

    Component for adding new estates initiated

---
+++ dotAngLandLord/ClientApp/src/app/create-new-estate/create-new-estate.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-new-estate',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      create-new-estate works!
+    </p>
+  `,
+  styleUrl: './create-new-estate.component.scss'
+})
+export class CreateNewEstateComponent {
+
+}
